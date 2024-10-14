fedd6557c2b7c5b84c9db4cf7826082e51e91657
Sammie0 <146608556+Shopifyexpertsam@users.noreply.github.com>
2024-10-14 09:52:09 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,52 +1,42 @@
-window.onload = () => {
-  const featuredProjectList = featuredProjects.map((project) => {
-    return `
-            <div class="bg-slate-100 p-3 rounded shadow-lg">
-                <div class="">
-                    <img src=${project.thumbnail} alt="hamburger food" class="w-full h-full">
-                </div>
-                <div class="flex justify-between items-center mt-3">
-                    <h1 class="text-slate-800 text-sm md:text-lg font-semibold">${project.name}</h1>
-                    <div class="flex justify-between items-center gap-3">
-                        <a class="text-sm md:text-base bg-slate-300 text-slate-800 py-1 px-3 transition hover:bg-slate-800 hover:text-slate-100 rounded"
-                            href=${project.code}
-                            target="_blank">Source Code</a>
-                        <a class="text-sm md:text-base bg-slate-300 text-slate-800 py-1 px-3 transition hover:bg-slate-800 hover:text-slate-100 rounded"
-                            href=${project.preview} target="_blank">Preview
-                            Demo</a>
-                    </div>
-                </div>
-            </div>
-        `;
-  });
+const start = document.getElementById('start');
+const stop = document.getElementById('stop');
+const reset = document.getElementById('reset');
+const timer = document.getElementById('timer');
 
-  document.getElementById("featured-projects").innerHTML =
-    featuredProjectList.join("");
-  const totalProjectList = totalProjects.map((project) => {
-    return `
-            <div class="bg-slate-100 p-3 rounded shadow-lg">
-                <div class="">
-                    <img src=${project.thumbnail} alt="hamburger food" class="w-full h-full">
-                </div>
-                <div class="flex justify-between items-center mt-3">
-                    <h1 class="text-slate-800 text-sm md:text-lg font-semibold">${project.name}</h1>
-                    <div class="flex justify-between items-center gap-3">
-                        <a class="text-sm md:text-base bg-slate-300 text-slate-800 py-1 px-3 transition hover:bg-slate-800 hover:text-slate-100 rounded"
-                            href=${project.code}
-                            target="_blank">Source Code</a>
-                        <a class="text-sm md:text-base bg-slate-300 text-slate-800 py-1 px-3 transition hover:bg-slate-800 hover:text-slate-100 rounded"
-                            href=${project.preview} target="_blank">Preview
-                            Demo</a>
-                    </div>
-                </div>
-            </div>
-    `;
-  });
 
-  document.getElementById("total-projects").innerHTML =
-    totalProjectList.join("");
+let interval;
 
-  setTimeout(() => {
-    document.getElementById("preloader").style.display = "none";
-  }, 2000);
-};
+let timeLeft = 1500;
+
+function update(){
+    let minutes = Math.floor(timeLeft / 60);
+    let seconds = timeLeft % 60 ;
+    let formattedTime =` ${minutes} :  ${seconds}`;
+
+    timer.innerHTML = formattedTime;
+}
+
+function startTimer(){
+    interval = setInterval(() => {
+        timeLeft--;
+        update();
+        if(timeLeft === 0){
+            alert('Times Up');
+            timeLeft = 1500;
+        }
+    }, 1000);
+}
+    
+
+function stopTimer(){
+    clearInterval(interval);
+}
+function resetTimer(){
+    clearInterval(interval);
+    timeLeft = 1500;
+    update();
+}
+
+start.addEventListener('click', startTimer);
+stop.addEventListener('click', stopTimer);
+reset.addEventListener('click', resetTimer);
